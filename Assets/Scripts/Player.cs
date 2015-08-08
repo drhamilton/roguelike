60b9325e7af1953eff932f12b88558fa97f067e4
Dillon <dillonrhamilton@gmail.com>
2015-08-08 14:09:06 -0400

    test

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,133 +2,128 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class Player : MovingObject {
+public class Player : MovingObject
+{
 
-    public int wallDamage = 1;
-    public int pointsPerFood = 10;
-    public int pointsPerSoda = 20;
-    public float restartLevelDelay = 1f;
+	public int wallDamage = 1;
+	public int pointsPerFood = 10;
+	public int pointsPerSoda = 20;
+	public float restartLevelDelay = 1f;
 
-    public AudioClip moveSound1;
-    public AudioClip moveSound2;
-    public AudioClip eatSound1;
-    public AudioClip eatSound2;
-    public AudioClip drinkSound1;
-    public AudioClip drinkSound2;
-    public AudioClip gameOverSound;
+	public AudioClip moveSound1;
+	public AudioClip moveSound2;
+	public AudioClip eatSound1;
+	public AudioClip eatSound2;
+	public AudioClip drinkSound1;
+	public AudioClip drinkSound2;
+	public AudioClip gameOverSound;
 
-    private GameObject foodTextGameObject;
-    private Text foodText;
+	private GameObject foodTextGameObject;
+	private Text foodText;
 
-    private Animator animator;
-    private int food;
+	private Animator animator;
+	private int food;
 
 	// Use this for initialization
 	protected override void Start ()
-    {
-        animator = GetComponent<Animator>();
+	{
+		animator = GetComponent<Animator> ();
 
-        food = GameManager.instance.playerFoodPoints;
+		food = GameManager.instance.playerFoodPoints;
 
-        foodTextGameObject = GameObject.FindWithTag("FoodText");
-        foodText = foodTextGameObject.GetComponent<Text>();
-        foodText.text = "Food: " + food;
+		foodTextGameObject = GameObject.FindWithTag ("FoodText");
+		foodText = foodTextGameObject.GetComponent<Text> ();
+		foodText.text = "Food: " + food;
 
-        base.Start();
+		base.Start ();
 
 	}
 
 
-    private void OnDisable()
-    {
-        GameManager.instance.playerFoodPoints = food;
-    }
+	private void OnDisable ()
+	{
+		GameManager.instance.playerFoodPoints = food;
+	}
 	// Update is called once per frame
 	void Update ()
-    {
-	   if (!GameManager.instance.playersTurn) return;
+	{
+		if (!GameManager.instance.playersTurn)
+			return;
 
-        int horizontal = 0;
-        int vertical = 0;
+		int horizontal = 0;
+		int vertical = 0;
 
-        horizontal = (int) Input.GetAxisRaw("Horizontal");
-        vertical = (int) Input.GetAxisRaw("Vertical");
+		horizontal = (int)Input.GetAxisRaw ("Horizontal");
+		vertical = (int)Input.GetAxisRaw ("Vertical");
 
-        if (horizontal != 0)
-            vertical = 0;
+		if (horizontal != 0)
+			vertical = 0;
 
-        if (horizontal != 0 || vertical != 0)
-            AttemptMove<Wall>(horizontal, vertical);
+		if (horizontal != 0 || vertical != 0)
+			AttemptMove<Wall> (horizontal, vertical);
 
 	}
 
-    protected override void AttemptMove<T>(int xDir, int yDir)
-    {
-        food = food - 1;
-        foodText.text = "Food: " + food;
-        base.AttemptMove<T>(xDir, yDir);
+	protected override void AttemptMove<T> (int xDir, int yDir)
+	{
+		food = food - 1;
+		foodText.text = "Food: " + food;
+		base.AttemptMove<T> (xDir, yDir);
 
-        RaycastHit2D hit;
-        if (Move(xDir, yDir, out hit))
-        {
-            SoundManager.instance.RandomizeSfx(moveSound1, moveSound2);
-        }
+		RaycastHit2D hit;
+		if (Move (xDir, yDir, out hit)) {
+			SoundManager.instance.RandomizeSfx (moveSound1, moveSound2);
+		}
 
-        CheckIfGameOver();
+		CheckIfGameOver ();
 
-        GameManager.instance.playersTurn = false;
-    }
+		GameManager.instance.playersTurn = false;
+	}
 
-    private void OnTriggerEnter2D (Collider2D other)
-    {
-        if(other.tag == "Exit")
-        {
-            Invoke("Restart", restartLevelDelay);
-            enabled = false;
-        }
-        else if (other.tag =="Food")
-        {
-            food += pointsPerFood;
-            foodText.text = "+" + pointsPerFood + " Food: " + food;
-            SoundManager.instance.RandomizeSfx(eatSound1, eatSound2);
-            other.gameObject.SetActive(false);
-        }
-        else if (other.tag == "Soda")
-        {
-            food += pointsPerSoda;
-            foodText.text = "+" + pointsPerSoda + " Food: " + food;
-            SoundManager.instance.RandomizeSfx(drinkSound1, drinkSound2);
-            other.gameObject.SetActive(false);
-        }
-    }
+	private void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.tag == "Exit") {
+			Invoke ("Restart", restartLevelDelay);
+			enabled = false;
+		} else if (other.tag == "Food") {
+			food += pointsPerFood;
+			foodText.text = "+" + pointsPerFood + " Food: " + food;
+			SoundManager.instance.RandomizeSfx (eatSound1, eatSound2);
+			other.gameObject.SetActive (false);
+		} else if (other.tag == "Soda") {
+			food += pointsPerSoda;
+			foodText.text = "+" + pointsPerSoda + " Food: " + food;
+			SoundManager.instance.RandomizeSfx (drinkSound1, drinkSound2);
+			other.gameObject.SetActive (false);
+		}
+	}
 
-    protected override void OnCantMove<T>(T component)
-    {
-        Wall hitWall = component as Wall;
-        hitWall.DamageWall(wallDamage);
-        animator.SetTrigger("PlayerChop");
-    }
+	protected override void OnCantMove<T> (T component)
+	{
+		Wall hitWall = component as Wall;
+		hitWall.DamageWall (wallDamage);
+		animator.SetTrigger ("PlayerChop");
+	}
 
-    private void Restart()
-    {
-        Application.LoadLevel(Application.loadedLevel);
-    }
+	private void Restart ()
+	{
+		Application.LoadLevel (Application.loadedLevel);
+	}
 
-    public void LoseFood(int loss)
-    {
-        animator.SetTrigger("playerHit");
-        food -= loss;
-        foodText.text = "-" + loss + " Food: " + food;
-        CheckIfGameOver();
-    }
+	public void LoseFood (int loss)
+	{
+		animator.SetTrigger ("playerHit");
+		food -= loss;
+		foodText.text = "-" + loss + " Food: " + food;
+		CheckIfGameOver ();
+	}
 
-    private void CheckIfGameOver()
-    {
-        if (food <= 0)
-        {
-            SoundManager.instance.PlaySingle(gameOverSound);
-            SoundManager.instance.musicSource.Stop();
-            GameManager.instance.GameOver();
-        }
-    }
+	private void CheckIfGameOver ()
+	{
+		if (food <= 0) {
+			SoundManager.instance.PlaySingle (gameOverSound);
+			SoundManager.instance.musicSource.Stop ();
+			GameManager.instance.GameOver ();
+		}
+	}
 }
